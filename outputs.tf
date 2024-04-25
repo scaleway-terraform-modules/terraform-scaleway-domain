93e36b774c63fae22a8e5f8f9f9f849b35ad79f0
Benoit Garcia <benoit.garcia@protonmail.com>
2024-04-25 23:35:48 +0200

    feat: Outputs status of the TEM domain.

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,11 @@ output "tem_reputation" {
   value       = try(scaleway_tem_domain.this[0].reputation, null)
 }
 
+output "tem_status" {
+  description = "Status of the Transaction Email Domain."
+  value       = try(scaleway_tem_domain.this[0].status, null)
+}
+
 output "tem_validated" {
   description = "Indicates if the domain has been verified for email sending."
   value       = try(scaleway_tem_domain_validation.this[0].validated, null)
