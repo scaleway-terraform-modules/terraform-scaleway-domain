5498ed4f1ac4d0a266f7d52646ddd2f2eba82bbc
Benoit Garcia <benoit.garcia@protonmail.com>
2025-10-01 07:40:31 +0200

    feat: Add support of BIMI logo

--- variables.tf
+++ variables.tf
@@ -32,3 +32,9 @@ variable "setup_tem" {
   description = "Whether to create a Transaction Email service. **Beware that the service is only available in region `fr-par` at the moment.**"
   default     = false
 }
+
+variable "bimi_logo_url" {
+  type        = string
+  description = "URL of the log that should be used for BIMI. The file must be a SVG document and follow [BIMI specifications](https://bimigroup.org/creating-bimi-svg-logo-files/)."
+  default     = null
+}
