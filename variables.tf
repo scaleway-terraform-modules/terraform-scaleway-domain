fe7e3938464587ebe6cb4a8c2f5594837ded50d2
Benoit Garcia <benoit.garcia@protonmail.com>
2025-10-01 07:40:31 +0200

    feat: Add support of BIMI logo

--- variables.tf
+++ variables.tf
@@ -3,6 +3,24 @@ variable "domain" {
   type        = string
 }
 
+variable "bimi_avatar_preference" {
+  type        = string
+  description = "Avatar Preference. Lets the Domain Owner express a preference to show either the BIMI logo or a sender’s personal avatar where both are supported."
+  default     = "brand"
+}
+
+variable "bimi_logo_url" {
+  type        = string
+  description = "URL of the log that should be used for BIMI. The file must be a SVG document and follow [BIMI specifications](https://bimigroup.org/creating-bimi-svg-logo-files/)."
+  default     = ""
+}
+
+variable "bimi_vmc_url" {
+  type        = string
+  description = "URL of the Verified Mark Certificate or Common Mark Certificate."
+  default     = ""
+}
+
 variable "mx_servers" {
   description = "Informations regarding MX servers. **MX servers are required to enable Transactional Emails.**"
   type = list(object({
